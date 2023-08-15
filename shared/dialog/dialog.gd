bf3d885ad063cb49af6cf3b4c963a18af3675ca3
vizualjack <67346906+vizualjack@users.noreply.github.com>
2023-08-15 20:12:15 +0200

    dialog system

--- shared/dialog/dialog.gd
+++ shared/dialog/dialog.gd
@@ -1,21 +1,77 @@
 extends Control
 class_name Dialog
 
-var diaPanel: Panel
+var textLabel: RichTextLabel
+
+var dialog_data
+var dialogStarted:bool = false
+signal onExit
+var answers
+
+
+func loadDialogData(dataPath:String):
+	var fileCont = _getFileContent(dataPath)
+	dialog_data = JSON.parse_string(fileCont)
+	if textLabel and not dialogStarted:
+		_loadNextPart()
+		
+func _getFileContent(filePath:String):
+	if not FileAccess.file_exists(filePath):
+		return ""
+	var file = FileAccess.open(filePath, FileAccess.READ)
+	return file.get_as_text()
+			
+func _loadNextPart():
+	if not "text" in dialog_data:
+		if "action" in dialog_data:
+			print("do action '", dialog_data["action"], "' with id '", dialog_data["id"], "'")
+		_closeDialog()
+		return
+	_showText(dialog_data["text"])
+	if "answers" in dialog_data:
+		answers = dialog_data["answers"]
+		_showAnswers(answers.keys())
+
+func _closeDialog():
+	onExit.emit()
+	WorldUtil.deleteDialog()
+	
+func _showAnswers(answers):
+	textLabel.add_text("\n\n")
+	for index in range(len(answers)):
+		var key = str(index+1)
+		var answer = answers[index]
+		textLabel.add_text(key + " - " + answer + "\n")
+
+func _showText(text:String):
+	textLabel.clear()
+	textLabel.add_text(text)
 
 func _ready():
-	diaPanel = $Panel
+	textLabel = $Panel/RichTextLabel
+	if not dialogStarted:
+		_loadNextPart()
 
 func _input(event):
 	if event is InputEventKey:
 		event = event as InputEventKey
 		if not event.pressed or event.echo:
 			return
-		var index = getIndex(event.keycode)
+		var index = _getIndex(event.keycode)
 		if index != null:
-			print("index: " + str(index))
+			_handleSelection(index)
 			
-func getIndex(keycode):
+func _handleSelection(index):
+#	if index == -1:   - for basic close on 0-Key
+#		_closeDialog()
+#		return
+	if index < len(answers.keys()):
+		print("index: " + str(index))
+		dialog_data = answers[answers.keys()[index]]
+		_loadNextPart()
+			
+func _getIndex(keycode):
+#	keycode >= 48 - for basic close on 0-Key
 	if keycode >= 49 and keycode <= 57:
 		return keycode - 49
 	return null
--- shared/scripts/worldUtil.gd
+++ shared/scripts/worldUtil.gd
@@ -5,12 +5,16 @@ var playerCam: Camera3D
 var currentDialog: Dialog
 var dialogScene = preload("res://shared/dialog/dialog.tscn")
 
-func createDialog():
+func createDialog(dialog_data_path:String) -> Dialog:
 	if currentDialog:
-		return
+		return null
 	currentDialog = dialogScene.instantiate()
+	currentDialog.loadDialogData(dialog_data_path)
 	add_child(currentDialog)
+	WorldUtil.player.isInConversation = true
+	return currentDialog
 
-func closeDialog():
+func deleteDialog():
 	remove_child(currentDialog)
 	currentDialog = null
+	WorldUtil.player.isInConversation = false
