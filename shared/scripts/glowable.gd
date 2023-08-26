a6130b03d4c1e12b4b87711d0012a365f09432ad
Maximilian Rich <maximilian.rich@online.de>
2023-08-26 18:18:55 +0200

    more natural flickering

--- shared/scripts/glowable.gd
+++ shared/scripts/glowable.gd
@@ -3,6 +3,10 @@ class_name Glowable
 extends CSGBox3D
 
 @onready var light = $light as OmniLight3D
+@onready var light_energy = light.light_energy
+@onready var light_indirect_energy = light.light_indirect_energy
+@onready var light_volumetric_fog_energy = light.light_volumetric_fog_energy
+@onready var emission_energy_multiplier = (material as StandardMaterial3D).emission_energy_multiplier
 @export var active = true
 @export var color = "c05239" as Color
 @export_category("Flicker Effect")
@@ -31,8 +35,19 @@ func lights_on():
 		material.emission = color
 	material.emission_enabled = true
 	light.visible = true
+	light.light_energy = light_energy
+	light.light_indirect_energy = light_indirect_energy
+	light.light_volumetric_fog_energy = light_volumetric_fog_energy
+	material.emission_energy_multiplier = emission_energy_multiplier
 
 func lights_off():
+	if flicker_natural:
+		if randf() <= 0.3:
+			light.light_energy = light_energy/3
+			light.light_indirect_energy = light_indirect_energy/3
+			light.light_volumetric_fog_energy = light_volumetric_fog_energy/3
+			material.emission_energy_multiplier = emission_energy_multiplier/3
+			return
 	material.emission_enabled = false
 	light.visible = false
 
