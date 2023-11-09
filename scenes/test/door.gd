5639732834531c94a2f2cc97ecce2d6ead846a6e
Maximilian Rich <maximilian.rich@online.de>
2023-11-09 21:23:18 +0100

    fix circular dependency exception

--- scenes/test/door.gd
+++ scenes/test/door.gd
@@ -2,7 +2,7 @@ extends Node3D
 
 class_name ProceduralDoor
 
-@export var map_options: Array[PackedScene]
+@export var map_options: Array[String]
 
 func get_any_map(rng: RandomNumberGenerator) -> PackedScene:
-	return map_options[rng.randi_range(0, map_options.size()-1)]
+	return load(map_options[rng.randi_range(0, map_options.size()-1)])
