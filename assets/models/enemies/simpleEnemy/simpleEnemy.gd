5ef0f9b32a23fc93abc9a1b4dddad1a18975b17c
vizualjack <67346906+vizualjack@users.noreply.github.com>
2023-11-21 19:30:47 +0100

    sight system

--- assets/models/enemies/simpleEnemy/simpleEnemy.gd
+++ assets/models/enemies/simpleEnemy/simpleEnemy.gd
@@ -3,7 +3,6 @@ class_name Enemy
 
 const SPEED = 3.0
 const ATTACK_RANGE = 1.7
-const VIEW_RANGE = 5
 const ROUTINE_RANGE = 2.5
 @onready var _anim_player = $visuals/mixamo_base/AnimationPlayer
 @onready var _anim_tree = $visuals/mixamo_base/AnimationTree
@@ -21,7 +20,8 @@ var state_machine = null
 var start_pos = null
 var routine_pos_use_time = 10000
 var rng = RandomNumberGenerator.new()
-@onready var base_ray_cast = $visuals/mixamo_base/Armature/Skeleton3D/viewRay/rayWrapper/RayCast3D
+@onready var sight_cone = $visuals/mixamo_base/Armature/Skeleton3D/sightAttachment/sightHolder/sightCone
+@onready var sight_raycast = $visuals/mixamo_base/Armature/Skeleton3D/sightAttachment/sightHolder/raycast
 
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -44,7 +44,6 @@ func _ready():
 	_shootable.setStartHealth(randi_range(1,10))
 	state_machine = _anim_tree.get("parameters/playback")
 	start_pos = position
-	base_ray_cast.enabled = false
 	rng.randomize()
 	
 func _get_player():
@@ -80,23 +79,23 @@ func _is_in_attack_range():
 		return false
 	return global_position.distance_to(player.global_position) < ATTACK_RANGE
 	
-func _is_in_view_range():
-	if not player:
-		return false
-	return global_position.distance_to(player.global_position) < VIEW_RANGE
+#func _is_in_view_range():
+#	if not player:
+#		return false
+#	return global_position.distance_to(player.global_position) < VIEW_RANGE
 	
 func _set_player_spotted_to_all_enemies(newSpottedFlag:bool):
 	var all_enemies = get_parent().find_children("*","Enemy") as Array[Enemy]
 	for enemy in all_enemies:
 		enemy.playerSpotted = true
 		
-func _try_to_spot_player():
-	if not base_ray_cast.is_colliding():
-		return
-	var col = base_ray_cast.get_collider()
-	if col is CharacterBody3D and not playerSpotted:
-		print("player spotted")
-		_set_player_spotted_to_all_enemies(true)
+#func _try_to_spot_player():
+#	if not base_ray_cast.is_colliding():
+#		return
+#	var col = base_ray_cast.get_collider()
+#	if col is CharacterBody3D and not playerSpotted:
+#		print("player spotted")
+#		_set_player_spotted_to_all_enemies(true)
 
 func _physics_process(delta):
 	if not player:
@@ -104,17 +103,10 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 	var next_pos = null
-	# need more raycasts
-#	base_ray_cast.enabled = (not playerSpotted and _is_in_view_range())
-	# temporary 
-	if _is_in_view_range():
-		_set_player_spotted_to_all_enemies(true)
-	####
 	if playerSpotted:
 		next_pos = _get_next_pos_to_player()
 	else:
 		next_pos = _get_next_routine_pos(delta)
-		_try_to_spot_player()
 	_anim_tree.set("parameters/conditions/has_target", next_pos != null)
 	_anim_tree.set("parameters/conditions/in_range", (playerSpotted and _is_in_attack_range()))
 	if next_pos and not dieing:
@@ -133,3 +125,27 @@ func _physics_process(delta):
 func _hit():
 	if _is_in_attack_range():
 		print("hitting...")
+		
+func _find_player(nodes:Array[Node3D]):
+	for node in nodes:
+		if node.name == "player":
+			return node
+	return null
+
+func _can_see(node:Node3D):
+	sight_raycast.look_at(node.global_transform.origin, Vector3.UP)
+	sight_raycast.force_raycast_update()
+	if not sight_raycast.is_colliding():
+		return false
+	var collider = sight_raycast.get_collider()
+	return (collider.name == "player")
+
+func check_sight():
+	var overlaps = sight_cone.get_overlapping_bodies()
+	if overlaps.size() <= 0:
+		return
+	var player = _find_player(overlaps)
+	if not player:
+		return
+	if _can_see(player):
+		_set_player_spotted_to_all_enemies(true)
