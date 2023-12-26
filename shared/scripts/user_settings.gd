6727a3a7ea93a4da5cc293ea1cf67768db5225e4
Maximilian Rich <maximilian.rich@online.de>
2023-12-26 15:34:00 +0100

    no debug prints; more resolutions

--- shared/scripts/user_settings.gd
+++ shared/scripts/user_settings.gd
@@ -5,7 +5,9 @@ const WINDOW_SIZES = [
 	Vector2(1280,720),
 	Vector2(1366,768),
 	Vector2(1600,900),
-	Vector2(1920,1080)
+	Vector2(1920,1080),
+	Vector2(2560,1440),
+	Vector2(3840,2160)
 ]
 
 const KEY_MOUSE_SENS = "mouse_sens"
